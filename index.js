3e8fb848c79471ae1195abc3ca9fe1ab2a06a390
Scott <scott.heron@outlook.com>
2016-02-14 21:38:01 -0800

    display the short address and the original address to the screen at the /link address

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*Link Shortening web app - generate a short address to replace the address 
+entered. Display the address to the user and redirect to the original link*/
+
+/*Main index.js file for the webserver, creates routes and handles traffic*/
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var ejsLayouts = require("express-ejs-layouts");
@@ -13,11 +18,16 @@ app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + "/static"));
 
-
+/*set up the home route and render index.ejs*/
 app.get("/", function(req, res){
 	res.render("index.ejs");
 });
 
+/*set up route /link and send the web address to the database. Once it gets there
+access the unique identifier it was given. pass that identifier to the hashid 
+algorithm which generates a short code based on the value of the ID. the short
+code is then send to the database, updating the entry for the web address. 
+the link.ejs file is then rendered*/ 
 app.post("/link", function(req, res){
 	var linkToShorten = req.body.linkToShorten;
 	db.linkToShorten.create({
@@ -28,7 +38,12 @@ app.post("/link", function(req, res){
 		  link.updateAttributes({
 		    short: id
 		  });	
-		  res.render("link/link.ejs");	  
+		  var address = link.link;
+		  var hashid = link.short;
+		  res.render("link/link.ejs", {
+		  	address: address,
+		  	hashid: hashid
+		  });	  
 		});
 	});
 });
