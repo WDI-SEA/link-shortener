0eeeba7ec6e647fac0f4f45d4047117a1bef5db6
abelmark <mabel16113@gmail.com>
2016-02-15 01:27:23 -0800

    working, needs styling

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var ejsLayouts = require('express-ejs-layouts')
 var router = express.Router();
 var Hashids = require('hashids');
 
-var hashids = new Hashids("Hash");
+var hashids = new Hashids("Hash", 6);
+
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,15 +18,51 @@ app.get('/', function(req, res){
 })
 
 app.post('/', function(req, res){
-	var link = (req.body.url);
-	console.log(link);
-  hash = hashids.encode(link);
-  console.log(hash);
-	db.hash.create(link, hash).then(function(hash){
-		res.redirect('/');
+	var link = req.body.url;
+	db.hash.findOrCreate({
+		where: 
+		{url: link}
+	}).spread(function(newRow, isNew){
+		console.log(newRow);
+		newRow.hash = hashids.encode(newRow.id);
+		newRow.clickCount = 0;
+		newRow.save();
+		res.redirect('/link/' + newRow.id);
 	})
-
 })
+
+app.get('/link/:id', function(req, res){
+	console.log(req.params.id);
+	db.hash.findById(req.params.id).then(function(link){
+		if(link){
+			res.render('link', {row : link})
+		}
+	})
+})
+
+app.get('/links', function(req, res){
+	db.hash.findAll({order: '"clickCount" DESC'}).then(function(links){
+		if(links){
+			res.render('links.ejs', {links : links});
+		}else{
+			res.send('error, ya fuck');
+		}
+	}).catch(function(err){
+		res.send('fucking error man');
+	})
+})
+
+app.get('/:hash', function(req, res){
+	var hashid = req.params.hash;
+		db.hash.findOne({where: {hash: hashid}
+		}).then(function(row){
+		row.clickCount += 1;
+		row.save();
+		res.redirect(row.url)
+	});
+});
+
+
 
 
 module.exports = router;
--- models/hash.js
+++ models/hash.js
@@ -2,7 +2,8 @@
 module.exports = function(sequelize, DataTypes) {
   var hash = sequelize.define('hash', {
     url: DataTypes.STRING,
-    hash: DataTypes.STRING
+    hash: DataTypes.STRING,
+    clickCount: DataTypes.INTEGER
   }, {
     classMethods: {
       associate: function(models) {
