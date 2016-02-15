8467cda7ed76847b09be1c1b05db50206674007e
Scott <scott.heron@outlook.com>
2016-02-15 11:29:23 -0800

    redirects to the address associated to the hash when user goes to /:hashid

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ entered. Display the address to the user and redirect to the original link*/
 
 /*Main index.js file for the webserver, creates routes and handles traffic*/
 
+/*self closing function to contain the code so as to isolate variables and 
+global variables. Closes at the end of the code.*/
+(function(){
+
 var express = require("express");
 var bodyParser = require("body-parser");
 var ejsLayouts = require("express-ejs-layouts");
 var db = require("./models");
 
+/*global variables*/
+var address;
+var hashid;
 var Hashids = require("hashids"),
 	hashids = new Hashids("this is my salt");
-
 var app = express();
 
 app.set("view engine", "ejs");
@@ -26,8 +32,9 @@ app.get("/", function(req, res){
 /*set up route /link and send the web address to the database. Once it gets there
 access the unique identifier it was given. pass that identifier to the hashid 
 algorithm which generates a short code based on the value of the ID. the short
-code is then send to the database, updating the entry for the web address. 
-the link.ejs file is then rendered*/ 
+code is then send to the database, updating the entry for the web address. The 
+address and generated hash id are then saved to global variables and there is a 
+redirect to /link/:id*/ 
 app.post("/link", function(req, res){
 	var linkToShorten = req.body.linkToShorten;
 	db.linkToShorten.create({
@@ -38,26 +45,31 @@ app.post("/link", function(req, res){
 		  link.updateAttributes({
 		    short: id
 		  });	
-		  var address = link.link;
-		  var hashid = link.short;
-		  res.render("link/link.ejs", {
-		  	address: address,
-		  	hashid: hashid
-		  });	  
+		  address = link.link;
+		  hashid = link.short;
+		  res.redirect("/link/"+hashid);
 		});
 	});
 });
 
-
-
+/*sets up a route for any webroute that has an id after link/ route. Once this path 
+is activated the link/link.ejs file is rendered and the global variables address and hashid are 
+passed to link.ejs and the file is rendered*/
 app.get("/link/:id", function(req, res){
-
+  	res.render("link/link.ejs", {
+	  address: address,
+	  hashid: hashid
+	});
 });
 
+/*sets up route for /:hash where the hashid is placed after the '/' and the user is redirected to
+the address associated with that hashid*/
 app.get("/:hash", function(req, res){
-
+   
+	  res.redirect("http://"+address);
 });
 
 
 app.listen(3000);
 
+}());
