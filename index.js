6b03af112d8b63407ff42e5c43fe10a7c80dd318
evwilkin <evan.wilkinson@gmail.com>
2016-02-16 09:59:52 -0800

    adding styling

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var	hashids = new Hashids('evwilkin');
  
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(__dirname + '/static'));
 
 //Load home page
 app.get('/', function(req, res) {
