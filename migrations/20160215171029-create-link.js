64617f2ea5c952a021dd2a6b025c70b2cadf1439
navyayvan <navyayvan@gmail.com>
2016-02-15 18:58:31 -0800

    routes finished

---
+++ migrations/20160215171029-create-link.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('links', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      hash: {
+        type: Sequelize.STRING
+      },
+      count: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('links');
+  }
+};
