8a9705677e5f4f903dfcf7c35496dd6d49ca70f6
Ella Erickson <ellazerickson@gmail.com>
2016-02-15 22:34:43 -0800

    link shortener so far, parts 1 and 2 complete

--- hashid.js
+++
@@ -1,9 +0,0 @@
-//example from class
-
-var Hashids = require('hashids');
-var hashids = new Hashids('salty random string');
-
-var newHash = hashids.encode(1234);
-
-console.log(newHash);
-console.log(hashids.decode(newHash));
