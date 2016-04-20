f28c99140e9e670e32c05ec71e0a92b761e6cd6b
brytree <brycebortree@gmail.com>
2016-04-19 22:20:11 -0700

    inserting npm and starting basic file structure

--- index.js
+++ index.js
@@ -9,3 +9,29 @@ app.set('view engine', 'ejs');
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended:false}));
+
+
+app.get("/", function(req, res) {
+  res.render('index');
+});
+
+app.post("/links", function(req, res) {
+  var comment = {imdbId: req.body.imdbID, 
+                  title: req.body.title, 
+                  year: req.body.year};
+
+  db.favorite.create(favMovie).then(function(movie){
+    console.log(movie);
+    res.redirect('/favs');
+  });
+});
+
+app.get("/links/:id", function(req, res) {
+  res.render('show');
+});
+
+app.get("/:hash", function(req, res) {
+  res.render('hash');
+});
+
+app.listen(3000);
