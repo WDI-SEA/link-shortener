a29735e9135534529ef842c026181c15b4040e86
Hannah Barnes <hlmbarnes@me.com>
2016-04-21 00:11:00 -0700

    fixed the post route for the shortner form to push to the database

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
-var express = require('express');
+var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var app = express();
+var db= require('./models');
+
+
 
 var app = express();
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(ejsLayouts);
-// app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res) {
 	res.render('index');
@@ -15,6 +18,26 @@ app.get('/', function(req, res) {
 // app.get('/shortner', function(req, res) {
 //   res.render('signups');
 // });
+
+app.get('/shortner', function(req, res) {
+	console.log("TEST");
+});
+
+app.post('/shortner', function(req, res){
+	console.log('TEST POST');
+	console.log(req.body);
+	console.log(req.body.url);
+
+	db.link.findOrCreate({
+		where: {
+			url: req.body.url,
+		},
+
+	})
+	 res.send(req.body);
+})
+
+
 
 var port = 3000;
 app.listen(port, function() {
