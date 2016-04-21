07f8b5ec75c42bba0c2909ed973329e662a407ba
Paul <pacalabre@gmail.com>
2016-04-20 18:44:53 -0700

    completed adding routes

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
-var ejsLayouts = require("express-ejs-layouts");
+
 var request = require('request');
 
 
 app.set('view engine', 'ejs');
-app.use(ejsLayouts);
 
 app.use(express.static(__dirname +'/views'));
 
 app.use( bodyParser.urlencoded({extended: false }) );
 
 app.get('/',function(req, res) {
-  res.send("hey");
+  res.render("index");
 })
 
+app.get('/links/:id',function(req, res ) {
+  res.render('links.ejs');
+})
 
+app.get('/links/:hash',function(req, res ) {
+  res.render('links.ejs');
+})
+
+app.post('/links', function(req,res) {
+
+})
 
 var port = 3000;
 app.listen(port, function() {
