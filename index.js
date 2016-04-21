c70ddf080bb71f0de94fe543e789eb2b61c0c82f
montehoover <monte.b.hoover@gmail.com>
2016-04-21 10:30:55 -0700

    hashes added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
 var flash = require('connect-flash');
 var session = require('express-session');
+var Hashids = require('hashids');
 var db = require('./models')
 
 var app = express();
+var hashids = Hashids('montehooversalt');
 
 app.set('view engine', 'ejs');
 app.use(ejsLayouts);
@@ -18,11 +20,24 @@ app.use(session({
 }));
 app.use(flash());
 
+var newUrl = 'ow.ly/1et13';
 
 app.use('/tweets', require('./controllers/tweet.js'));
 
+// Both the home page and the show route
 app.get('/', function(req, res) {
-  res.render('index');
+  res.render('index', {url: newUrl});
+});
+
+app.post('/', function(req, res) {
+  res.send(req.body);
+  hashedUrl = hashids.encode(req.body.fullUrl)
+  db.link.create({url: hashedUrl})
+});
+
+app.get('/:hash', function(req, res) {
+  var url = hashids.decode(req.params.hash)
+  db.findOne(:)
 });
 
 app.get('/auth/signup', function(req, res) {
