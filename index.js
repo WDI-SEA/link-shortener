01f3f868b17c682d02cbc83ccde6a2ceb73b36a4
Hannah Barnes <hlmbarnes@me.com>
2016-04-20 22:39:04 -0700

    updated the formatting of the form and the placeholers in the form for both the url and the resulting shortURL

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.get('/', function(req, res) {
 	res.render('index');
 });
 
+// app.get('/shortner', function(req, res) {
+//   res.render('signups');
+// });
+
 var port = 3000;
 app.listen(port, function() {
   console.log("You're listening to the smooth sounds of port " + port);
