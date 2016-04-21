78fc7569d8887eedc909cb144e521fd62dd4a58f
Paul <pacalabre@gmail.com>
2016-04-20 20:09:23 -0700

    able to add link to database

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 var express = require("express");
 var bodyParser = require('body-parser');
 var app = express();
-
 var request = require('request');
+var mongoose = require('mongoose');
 
+var Url = require('./models/links');
+//Hash
+var Hashids = require("hashids"),
+  hashids = new Hashids("this is my salt");
 
 app.set('view engine', 'ejs');
 
@@ -11,12 +15,26 @@ app.use(express.static(__dirname +'/views'));
 
 app.use( bodyParser.urlencoded({extended: false }) );
 
+
+mongoose.connect('mongodb://localhost/link-shortener');
+
+
+//
+
 app.get('/',function(req, res) {
   res.render("index");
 })
 
 app.get('/links/:id',function(req, res ) {
-  res.render('links.ejs');
+  var id = req.params.id;
+  console.log(id);
+  res.render('links.ejs',{id:id});
+})
+
+app.get('/links',function(req, res ) {
+  var id = req.params.id;
+  console.log(id);
+  res.render('links.ejs',{id:id});
 })
 
 app.get('/links/:hash',function(req, res ) {
@@ -24,8 +42,12 @@ app.get('/links/:hash',function(req, res ) {
 })
 
 app.post('/links', function(req,res) {
+  formSubmit = req.body;
+  Url.create({url:formSubmit.url});
 
 })
+
+
 
 var port = 3000;
 app.listen(port, function() {
