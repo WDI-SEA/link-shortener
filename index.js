0944309041d7e5594cccbde9faaba03dc50d8c1f
keidra <keidrabaldwin@gmail.com>
2016-04-21 09:24:46 -0700

    initial files created, had to delete database due to errors

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
+var flash = require
+var db = require('./models');
+
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(__dirname + '/static'));
+
+
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+
+
+
+
+
+
+
+
+
+var port = 3000;
+app.listen(port, function() {
+  console.log("You're listening to the smooth sounds of port " + port);
+});
