05a1cbe70bf3e128ea7da12c9f52312bb5ed8fa6
Hannah Barnes <hlmbarnes@me.com>
2016-04-20 22:30:11 -0700

    updated the basic format to have the page with the form and button to shorten as well as basic databasic framework with URL and hash

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
+var express = require('express');
+var ejsLayouts = require('express-ejs-layouts');
 
+var app = express();
+app.set('view engine', 'ejs');
+app.use(ejsLayouts);
+// app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(__dirname + '/static'));
 app.get('/', function(req, res) {
-  res.send('welcome to twitter');
+	res.render('index');
 });
 
 var port = 3000;
