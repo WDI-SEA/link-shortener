1f796df63d6ed19863106a74124917a35edd4c64
kedeger <kedeger@uw.edu>
2016-08-15 19:56:24 -0700

    got basic pages and routes working, no hash

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var ejsLayouts = require('express-ejs-layouts');
 
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('main/index.ejs');
+
 });
+
+app.use('/links', require('./controllers/links'));
 
 var server = app.listen(process.env.PORT || 3000);
 
