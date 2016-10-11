ea374cda94bcd4fdd117774d7c75aef73a1ce801
jamiebb <jamieandburton@gmail.com>
2016-10-11 08:59:43 -0700

    An attempt...

--- index.js
+++ index.js
@@ -1,10 +1,51 @@
 var express = require('express');
 var app = express();
+var ejsLayouts = require('express-ejs-layouts');
+var Hashids = require('hashids');
+var hashids = new Hashids('Testing...');
+
+app.set('view engine', 'ejs');
+
+app.use(ejsLayouts);
 
 app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('index');
+});
+
+app.post('/', function(req, res) {
+	var box = req.body.urlBox;
+	if (box) {
+		res.send(box);
+	}
+});
+
+// Accepts data from the form
+
+app.post('/links', function(req, res) {
+
+	console.log(shorten);
+
+	var small = hashids.encode(req.body.shorten);
+
+	db.links.create( {
+		url: small
+	}).then(function(link) {
+		if (link) {
+			res.redirect('index');
+		} else {
+			res.send('An error occurred.');
+		}
+	})
+});
+
+app.get('/links', function(req, res) {
+	res.render('links');
+});
+
+app.get('/:hash', function(req, res) {
+	res.redirect('');
 });
 
 var server = app.listen(process.env.PORT || 3000);
