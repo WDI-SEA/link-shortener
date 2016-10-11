b4e94d13aee2d6f401dcedc5e41eca69215337a7
tannerkovach <kovach.tanner@gmail.com>
2016-10-11 09:49:30 -0700

    85 - 90% working

--- index.js
+++ index.js
@@ -1,12 +1,55 @@
 var express = require('express');
+var bodyParser = require("body-parser");
+var ejsLayouts = require("express-ejs-layouts");
 var app = express();
+var db = require("./models");
+var Hashids = require('hashids');
+var hashids = new Hashids("whatever", 5);
 
 app.use(require('morgan')('dev'));
+app.set("view engine", "ejs");
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({extended: false}));
 
+
+//form
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render("home");
+  
+
 });
 
+// console.log(req.body);
+	// res.send("ayy");
+	// storess url in the database - redirects
+app.post('/links', function(req, res){
+	db.link.create({
+		url: req.body.urlInput
+	}).then(function(link){
+		res.redirect("/links/" + link.id);
+	});
+});
+
+//show link to route
+app.get('/links/:id', function(req, res){
+	db.link.findOne({
+		where: {id: req.params.id}
+	}).then(function(link){
+		var hash = hashids.encode(link.id);
+		res.render("redirect", {link: link, hash: hash});
+	});
+});
+
+// decode then take you to actual url
+app.get('/:hash', function(req, res){
+	var unhashed = hashids.decode(req.params.hash);
+	db.link.findOne({
+		where: {id: unhashed[0]}
+	}).then(function(link, err){
+		res.redirect(link.dataValue.url);
+	});	 
+});
 var server = app.listen(process.env.PORT || 3000);
 
 module.exports = server;
+
