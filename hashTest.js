b73b0b152d5614be56ee2dcf44ffb2e891f72f28
brianbixby0@gmail.com <brianbixby0@gmail.com>
2017-01-03 19:16:04 -0800

    works

---
+++ hashTest.js
@@ -0,0 +1,12 @@
+// // require and create a new Hashids object
+// var Hashids = require("hashids");
+// var hashids = new Hashids("this is my salt");
+//
+// // turn a number (such as a model id) to a hash
+// var hash = hashids.encode(123456789); // hash is now "NkK9"
+//
+// // decode the hash back into a number (note that this returns an array)
+// var linkId = hashids.decode(hash); // linkId is now [ 12345 ]
+//
+// console.log(hash);
+// console.log(linkId);
--- index.js
+++ index.js
@@ -7,6 +7,10 @@ var moment = require('moment');
 //global variables
 var app = express();
 var db = require("./models");
+
+var Hashids = require("hashids");
+var hashids = new Hashids("The 256 -bit naCl for the hash!");
+
 //set /use statements
 app.set("view engine", "ejs");
 app.use(ejsLayouts);
@@ -26,17 +30,44 @@ app.use(function(req, res, next) {
 //routes
 //Homepage
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('home');
 });
-// gets all articles  db. is based on folder models name
-//get single article by ID
-//get new article form
-// create a new article
 
-
+app.post('/links', function(req, res) {
+  db.link.create(req.body).then(function(link) {
+    res.redirect('/links/' + link.id);
+  });
+});
+app.get('/links/:id', function(req, res) {
+  db.link.find({
+      where: {
+        id: req.params.id
+      },
+    })
+    .then(function(link) {
+      var hash = hashids.encode(link.id);
+      console.log("Hash is " + hash);
+      res.render('links', {
+        hash: hash
+      });
+    });
+});
+app.get('/:hash', function(req, res) {
+  console.log(req.params.hash);
+  var hash = hashids.decode(req.params.hash);
+  console.log("decoded has is " + hash);
+  db.link.find({
+      where: {
+        id: hash
+      },
+    })
+    .then(function(link) {
+      res.redirect(link.url);
+    });
+});
 
 // bring in controllers
-app.use('/authors', require('./controllers/authors'));
+// app.use('/authors', require('./controllers/name'));
 //listen
 var server = app.listen(process.env.PORT || 3000);
 module.exports = server;
