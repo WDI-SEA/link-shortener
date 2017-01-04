7f18a01fef28786be77406696a1d91af808349fc
Tommy-Lim <tlim24@gmail.com>
2017-01-04 08:28:19 -0800

    changed formatting to counter and link page

--- index.js
+++ index.js
@@ -47,16 +47,17 @@ app.get('/:hash',function(req,res){
       link.count++;
       console.log('count more than 0: '+link.count);
       link.save().then(function(){
-        res.send('link url and count is: '+link.url+' '+link.count);
+        // res.send('link url and count is: '+link.url+' '+link.count);
+        res.render('outbound',{link: link});
       });
     } else{
       link.count = 1;
       console.log('count more than 0: '+link.count);
       link.save().then(function(){
-        res.send('link url and count is: '+link.url+' '+link.count);
+        // res.send('link url and count is: '+link.url+' '+link.count);
+        res.render('outbound',{link: link});
       });
     }
-    // res.send('link url is: '+link.url);
   });
 });
 
