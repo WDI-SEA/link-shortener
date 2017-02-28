07df354d6d8aaafb72ad501ee86eacd7b0b19a5e
themartinsjoseph <themartinsjoseph@gmail.com>
2017-02-28 06:34:23 -0800

    app setup complete

--- index.js
+++ index.js
@@ -1,12 +1,35 @@
+//Requires & Global Vars
 var express = require('express');
+var request = require('request');
+var ejsLayouts = require('express-ejs-layouts');
+var bodyParser = require('body-parser');
 var app = express();
 
+//Set & Use Statements
+app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
+app.use(ejsLayouts);
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+//Routes
+app.get('/', function(req,res) {
+res.send('Hello Backend!');
 });
 
-var server = app.listen(process.env.PORT || 3000);
+app.post('/links', function(req,res){
+//
+});
 
+app.get('/links/:id', function(req,res){
+//
+});
+
+app.get('/:hash', function(req,res){
+//
+});
+
+
+
+//Listener
+var server = app.listen(process.env.PORT || 3000);
 module.exports = server;
