fef0c5c01a5fc7d052ebc54099e2c58ec1fb8f74
themartinsjoseph <themartinsjoseph@gmail.com>
2017-02-28 08:19:56 -0800

    routes created with bugs

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 //Requires & Global Vars
 var express = require('express');
-var request = require('request');
+var db = require('./models');
+// var request = require('request');
 var ejsLayouts = require('express-ejs-layouts');
 var bodyParser = require('body-parser');
+var Hashids = require('hashids');
+var hashids = new Hashids('this is my salt');
 var app = express();
 
 //Set & Use Statements
@@ -10,26 +13,46 @@ app.set('view engine', 'ejs');
 app.use(require('morgan')('dev'));
 app.use(ejsLayouts);
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
 
 //Routes
-app.get('/', function(req,res) {
-res.send('Hello Backend!');
 //Contains a simple form where a user can enter a URL and get a short URL
+app.get('/', function(req,res) {
+res.render('index');
 });
 
-app.post('/links', function(req,res){
 //Accepts data from the form. Stores the URL in the database and redirects to the show route.
+app.post('/links', function(req,res){
+	db.link.create({
+		url: req.body.url
+	}).then(function(link) {
+		res.redirect('/links' + link.id);
+	}).catch(function(err) {
+		res.send({ message: 'error', error: err })
+	});
 });
 
-app.get('/links/:id', function(req,res){
 //Displays the short URL of the specified id (so the user can copy / share it)
+app.get('/links/:id', function(req,res){
+	db.link.find({
+		where: {
+			id: req.params.id
+		},
+	}).then(function(link) {
+	var linkId = hashids.encode(link.id);
+	res.render('links', {linkId:linkId});	
+	});
 });
 
-app.get('/:hash', function(req,res){
 //Takes the hash and redirects the user to the URL stored in the database.
+app.get('/:hash', function(req,res){
+var linkId = hashids.decode(req.params.hash);
+db.link.find({
+	where: {id: linkId}
+}).then(function(link){
+	res.redirect('http://' + link.link)
+})
 });
-
-
 
 //Listener
 var server = app.listen(process.env.PORT || 3000);
