0895c7e0f3edfae79316462e0d4299b04da2e08f
CannedYeti <cpotebnya@gmail.com>
2017-02-28 06:42:40 -0800

    finished product

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 var express = require('express');
+var bodyParser = require("body-parser");
+var ejsLayouts = require('express-ejs-layouts');
+var db = require('./models');
+var Hashids = require("hashids");
+var hashids = new Hashids("this is my salt");
 var app = express();
 
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(ejsLayouts)
 app.use(require('morgan')('dev'));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render('home');
 });
+
+app.post("/links", function(req, res) {
+	db.link.create(req.body)
+	.then(function(link) {
+		res.redirect('./links/' + link.id);
+	})
+});
+
+app.get('/links/:id', function(req, res) {
+	var hash = hashids.encode(req.params.id);
+	var hashObj = {
+		id: hash
+	}
+	db.link.find({
+		where: {
+			id: req.params.id
+		}
+	}).then(function(links) {
+		res.render('links', {links: links, hashObj: hashObj});	
+	})
+});
+
+app.get('/:hash', function(req, res) {
+	var linkId = hashids.decode(req.params.hash);
+	db.link.find({
+		where: {
+			id: linkId
+		}
+	}).then(function(links) {
+		res.redirect("http://" + links.link);
+	})
+});
+
+
 
 var server = app.listen(process.env.PORT || 3000);
 
