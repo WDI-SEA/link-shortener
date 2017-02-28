e4bc81f4025dbde92f10281b9d3332a517cdfcd5
jnhustin <jnhustin@gmail.com>
2017-02-27 19:07:58 -0800

    laid out basic express skeleton

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
+// global variables and requires
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
-app.use(require('morgan')('dev'));
 
+
+
+// use and set statements
+app.use(require('morgan')('dev'));
+app.use(express.static(__dirname + '/public'));
+app.use(ejsLayouts);
+
+app.set('view engine', 'ejs');
+
+// routes
+
+// get, root view
 app.get('/', function(req, res) {
   res.send('Hello Backend!');
 });
+
+// post, site w/ form to create link
+
+
+// get, show link
+
+
+// get, redirect
+
+
+
+// listen
 
 var server = app.listen(process.env.PORT || 3000);
 
