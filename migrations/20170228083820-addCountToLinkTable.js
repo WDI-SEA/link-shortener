f81ae35807f58b2bd7f81e24f73a1c24fb5e1e02
cgnzlz22 <cgnzlz22@yahoo.com>
2017-03-01 00:58:26 -0800

    cgonzalez github hates me

---
+++ migrations/20170228083820-addCountToLinkTable.js
@@ -0,0 +1,28 @@
+'use strict';
+
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('links', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('links');
+  }
+};
