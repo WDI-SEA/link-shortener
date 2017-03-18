cbc3f0ef460d87cc25a50f7b46ef2203ccd56216
kellyh6 <faithkeeping@yahoo.com>
2017-03-18 10:49:19 -0700

    most recent version

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 var express = require('express');
-// var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
+var Hashids = require("hashids");
+var sequelize = require('sequelize');
+
+
 var app = express();
+var db = require("./models");
+var hashids = new Hashids("this is my salt");
 
 // app.use(require('morgan')('dev'));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
 
+
+//home
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  res.render("layout");
+});
+
+//create link
+//stort in the database
+//hash in the promise
+app.post('/links', function(req, res) {
+  db.link.create({
+    url: req.body.url
+  }).then(function(link){
+    res.redirect('/links' + link.id);
+  });
+});
+
+
+//show link
+//incorporate hash here
+app.get("/links/:id", function(req, res) {
+	var hash = hashids.encode(req.params.id);
+	res.render("show", { hash: hash });
+});
+
+//redirect
+app.get("/:hash", function(req, res) {
+	db.link.findOne({
+		where: {id: hashids.decode(req.params.hash)}
+	}).then(function(link) {
+		if(link) {
+			res.redirect(link.url);
+		} else {
+			res.send("an error occured reading your short link.")
+		}
+	})
 });
 
 var server = app.listen(process.env.PORT || 3000);
