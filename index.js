e799f834c9d02e684d22ccd92c070e08181b5737
jbadan <jenna.badanowski@gmail.com>
2017-09-26 09:19:15 -0700

    fixes

--- index.js
+++ index.js
@@ -13,19 +13,21 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 app.use(express.static(__dirname + '/public'));
 
-
+//renders homepage
 app.get('/', function(req, res) {
     res.render('index');
 });
 
+//grabs info from input on form to add to database
 app.post('/link', function(req,res){
   db.link.findOrCreate({
     where: {url: req.body.url},
   }).spread(function(newUrl, wasCreated){
     res.redirect('link/' + newUrl.id);
   });
- });
+});
 
+//gets a page with list of all links in database
 app.get('/link', function(req,res){
   db.link.findAll({order: 'id DESC'}).then(function(result){
     result.forEach(function(link){
@@ -35,16 +37,17 @@ app.get('/link', function(req,res){
   });
 });
 
+//show individual page(hash.ejs) for each link in database
 app.get('/link/:id', function(req, res){
   var hash = hashids.encode(parseInt(req.params.id));
   res.render('hash', {hash: hash});
 });
 
+//this redirects to hash page aka original page
 app.get('/:hash', function(req, res){
   var linkId = parseInt(hashids.decode(req.params.hash));
   db.link.findById(linkId).then(function(result) {
-    console.log(result.url);
-    res.redirect("https://"+result.url+"");
+    res.redirect(result.url);
    });
 });
 
